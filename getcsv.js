a54d70363a6e578918fabaa9acb47db56f8e421e
mioni <78632013+yuryr-kili@users.noreply.github.com>
2022-12-07 14:25:37 +0900

    csvを持ってくるテスト

---
+++ getcsv.js
@@ -0,0 +1,13 @@
+const outputElement = document.getElementById('output_csv');
+
+function getCsvData(dataPath) {
+ const request = new XMLHttpRequest();
+ request.addEventListener('load', (event) => {
+  const response = event.target.responseText;
+  outputElement.innerHTML = response;
+ });
+ request.open('GET', dataPath, true);
+ request.send();
+}
+
+getCsvData('./testdata.csv');
