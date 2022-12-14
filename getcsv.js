43f758e686f7597f11ef48d3b9e68afe9dcabc7e
mioni <78632013+yuryr-kili@users.noreply.github.com>
2022-12-14 12:54:31 +0900

    正規表現

--- getcsv.js
+++ getcsv.js
@@ -5,6 +5,7 @@ function csv_data(dataPath) {
 	const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
 	request.addEventListener('load', (event) => { // ロードさせ実行
 		const response = event.target.responseText; // 受け取ったテキストを返す
+		response.replace(/\s+/g, ""); //空白削除
 		output_svg.innerHTML = response; // 表示
 	});
 	request.open('GET', dataPath, true); // csvのパスを指定
