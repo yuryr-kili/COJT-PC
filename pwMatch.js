95b37bd0d91745ff277d16fd42f3a622f0917f59
Chiji0503 <chijitai@gmail.com>
2022-12-16 15:34:37 +0900

    test

--- pwMatch.js
+++ pwMatch.js
@@ -1,6 +1,6 @@
 var pwlist=new array(1234,5678,9012,3456,7890);
 function pw_check() {
-    if(items.indexOf(1234)=2){
+    if(pwlist.indexOf(1234)=2){
         alert("correct pw");
     }
     else{
