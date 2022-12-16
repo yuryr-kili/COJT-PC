62fd9f52a2e4e8614f6295c7bffe3b3d8ebef676
Chiji0503 <chijitai@gmail.com>
2022-12-16 17:25:27 +0900

    pwの場所をhtmlで書いて引数でちゃんととるようにした

--- pwMatch.js
+++ pwMatch.js
@@ -1,6 +1,6 @@
-var pwlist=Array.of(1234,5678,9012,3456,7890);
-function pw_Match() {
-    if(pwlist.indexOf(1234)==-1){
+var pw=Array.of(1234,5678,9012,3456,7890);
+function pw_Match(pw) {
+    if(pw.indexOf(1234)==-1){
         alert("incorrect pw");
     }
     else{
