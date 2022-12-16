61b794b3086bba86f75eee6d09f72f58bf069988
kili <78632013+yuryr-kili@users.noreply.github.com>
2022-12-16 15:35:23 +0900

    add dictionary attack

--- dictionaryAttack.js
+++ dictionaryAttack.js
@@ -1,15 +1,12 @@
-const output_svg = document.getElementById('color_list');
-let index = 1;
-
-function csv_data(dataPath) {
+function get_csvdata(dataPath) {
 	const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
+    let list;
 	request.addEventListener('load', (event) => { // ロードさせ実行
 		const response = event.target.responseText; // 受け取ったテキストを返す
-		response.replace(/\s+/g, ""); //空白削
-		const list = response.split(",");
-		console.log(list);
-		output_svg.innerHTML = response; // 表示
+		response.replace(/\s+/g, ""); //空白削除
+		list = response.split(",");
 	});
 	request.open('GET', dataPath, true); // csvのパスを指定
 	request.send();
+    return list;
 }
