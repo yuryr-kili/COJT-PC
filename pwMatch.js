a2d64a1703bb2e9aa0a32c14ba787ad0a6d7a937
Chiji0503 <chijitai@gmail.com>
2022-12-21 12:53:35 +0900

    関数の仕様を仕様書通りに書いた

--- pwMatch.js
+++ pwMatch.js
@@ -1,6 +1,6 @@
 var pw=Array.of(1234,5678,9012,3456,7890);
-function pw_Match(pw) {
-    if(pw.indexOf(1234)==-1){
+function pw_Match(pw,correct) {
+    if(pw.indexOf(correct)==-1){
         alert("incorrect pw");
     }
     else{
