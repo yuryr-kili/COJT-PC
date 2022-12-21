7185a3e9f08cf14726a9d5da667391257778422a
kili <78632013+yuryr-kili@users.noreply.github.com>
2022-12-21 12:38:21 +0900

    ちょっと修正

--- dictionaryAttack.js
+++ dictionaryAttack.js
@@ -1,14 +1,14 @@
 function get_csvdata(dataPath) {
 	const request = new XMLHttpRequest(); // HTTPでファイルを読み込む
     let list;
+	let array_dic;
 	request.addEventListener('load', (event) => { // ロードさせ実行
 		const response = event.target.responseText; // 受け取ったテキストを返す
 		response.replace(/\s+/g, ""); //空白削除
 		list = response.split(",");
-		var array_dic = Array(list);
+		array_dic = Array(list);
 		return array_dic;
 	});
 	request.open('GET', dataPath, true); // csvのパスを指定
 	request.send();
-    return list;
 }
